ee73eda0d16a0b7fe0eb2dfe51d7650d83db8666
maseyyi <32487927+maseyyi@users.noreply.github.com>
2020-04-20 06:34:44 +0700

    Update terkey.py

--- terkey.py
+++ terkey.py
@@ -18,8 +18,8 @@ def setup():
 
 def banner():
     os.system('clear')
-    print(a+'Shortcut for help you'.center(40))
-    print(b+'Karjok Pangesty'.center(40))
+    print(a+'Shortcut untuk menampilkan tombol default aplikasi Termux'.center(40))
+    print(b+'Heri Kurnianto'.center(40))
     print("".join([i for i in "\n"*3]))
 
 
@@ -30,10 +30,10 @@ if __name__=='__main__':
     t.start()
     while t.is_alive():
         for i in "-\|/-\|/":
-            print('\rPlease wait '+i+' ',end="",flush=True)
+            print('\rPlease wait'+i+' ',end="",flush=True)
             sleep(0.1)
     banner()
-    print(c+'Silahkan hubungi '+a+'https://t.me/om_karjok'+c+' jika ada yang mau di bicarakan terkait tool ini, bisnis atau sekedar bertanya kabar. :v\nTerimakasih ^_^')
+    print(c+'Follow di'+a+'https://t.me/pochiel'+c+' untuk menjalin persahabatan. :v\nTerimakasih ^_^')
 
 # ini cuma shortcut buat bantu para nub
-# karjok pangesty
+# Heri Kurnianto
